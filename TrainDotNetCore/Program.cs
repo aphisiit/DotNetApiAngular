236de72e5acab2ed377229db2016e896b6b4eb54
Aphisit Namracha <aphisiit086757@hotmail.com>
2019-02-09 20:35:48 +0700

    Add how to read data from excel file.

--- TrainDotNetCore/Program.cs
+++ TrainDotNetCore/Program.cs
@@ -19,6 +19,12 @@ namespace TrainDotNetCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                 .ConfigureLogging(logging =>
+                 {
+                     logging.AddConsole();
+                     logging.AddDebug();
+                     logging.AddEventSourceLogger();                     
+                 });
     }
 }
